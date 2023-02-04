792353d503590be4539e6a5931027ae1a467e50e
Neetu981 <neetup281@gmail.com>
2023-02-04 14:29:57 +0530

    dom2

---
+++ dom/dom2.js
@@ -0,0 +1,10 @@
+var list=document.getElementsByTagName('li') 
+for(var i=0;i<list.length;i++)
+{
+    list[i].style.fontWeight='bold'
+}
+var items=document.getElementsByClassName('list-group-item')
+for(var i=0;i<list.length;i++)
+{
+    list[i].style.color='blue'
+}
